4dcc9685c51ef373f8b65da06a878cf14d3cdd20
Anshdeep Shrivastav <anshshrivastav032@gmail.com>
2025-05-14 13:47:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="underline font-bold text-4xl ">
+        <h1>Hello Team!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
