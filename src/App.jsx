2e521b34f9c86e56b6142f88551417414a78c020
mayurnanwani <mayurnanwani17@gmail.com>
2025-05-27 15:09:50 +0530

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 
-import './App.css'
+import { Route, BrowserRouter as Router, Routes, Link } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
+import Project from './components/projects/Project';
+import Events from './components/events/Events';
+import Team from './components/team/Team';
+import Story from './components/story/Story';
+import Footer from './components/footer/Footer';
+
 
 function App() {
 
   return (
     <>
-      <div className="underline font-bold text-4xl ">
-        <h1>Hello Team!</h1>
-      </div>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+					<Route path="/projects"element={<Project/>} />
+					<Route path="/events" element={<Events/>} />
+					<Route path="/team" element={<Team/>} />
+					<Route path="/story" element={<Story/>} />
+					
+        </Routes>
+      </Router>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/events/Events.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Events = () => {
+  return (
+    <div>
+      I am Event
+    </div>
+  )
+}
+
+export default Events
