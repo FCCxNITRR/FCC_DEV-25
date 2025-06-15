ca9a81029c1f95f17ebfd0933fa39228353fe427
DevKnight4 <swara.mandale7@gmail.com>
2025-06-15 19:17:58 +0530

    Moved BrowserRouter to main.jsx , set bg color and renamed story => almni

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 
-import { Route, BrowserRouter as Router, Routes, Link } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom'; //BrowserRouter as Router,
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
 import Project from './components/projects/Project';
 import Events from './components/events/Events';
 import Team from './components/team/Team';
-import Story from './components/story/Story';
+import Alumni from './components/alumni/Alumni';
 import Footer from './components/footer/Footer';
 
 
 function App() {
 
   return (
-    <>
+    <div className="min-h-screen bg-[#0A0E17]">
       <Navbar />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-					<Route path="/projects"element={<Project/>} />
-					<Route path="/events" element={<Events/>} />
-					<Route path="/team" element={<Team/>} />
-					<Route path="/story" element={<Story/>} />
-					
-        </Routes>
-      </Router>
-      <Footer/>
-    </>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/alumni" element={<Alumni />} />
+
+      </Routes>
+
+      <Footer />
+    </div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
   return (
-    <div>
-      I am Navbar
-    </div>
+    <nav className="bg-[#0A0E17] shadow-sm px-10 py-7 flex items-center justify-between">
+      {/* Logo and Name */}
+      <Link to="/">
+        <div className="flex items-center space-x-3">
+          <img
+            src="/logo.png"
+            alt="FCC Logo"
+            className="h-10 w-10 object-contain"
+          />
+          <div className="text-left leading-tight">
+            <h1 className="text-lg font-semibold text-white ">Finance and Consulting Club</h1>
+            <p className="text-md text-gray-200">NIT Raipur</p>
+          </div>
+        </div>
+      </Link>
+
+      {/* Navigation Links */}
+      <div className="flex space-x-15 text-lg font-medium text-gray-100 pr-10">
+        <Link to="/team" className="hover:text-blue-400">About Us</Link>
+        <Link to="/events" className="hover:text-blue-400">Events</Link>
+        <Link to="projects" className="hover:text-blue-400">Projects</Link>
+        <Link to="/contact" className="hover:text-blue-400">Contact Us</Link>
+        <Link to="/alumni" className="hover:text-blue-400">Alumni Corner</Link>
+      </div>
+
+      <div className="peer-checked:flex hidden flex-col space-y-4 px-8 pb-6 md:hidden text-lg font-medium">
+        <Link to="/team" className="hover:text-blue-400">About Us</Link>
+        <Link to="/events" className="hover:text-blue-400">Events</Link>
+        <Link to="/projects" className="hover:text-blue-400">Projects</Link>
+        <Link to="/contact" className="hover:text-blue-400">Contact Us</Link>
+        <Link to="/alumni" className="hover:text-blue-400">Alumni Corner</Link>
+      </div>
+
+    </nav>
   )
 }
 
